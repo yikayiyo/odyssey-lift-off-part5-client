39e59c0e8631f0ffd8d8565337866109938b2cfd
yikayiyo <837937787@qq.com>
2021-10-20 13:48:14 +0800

    update client uri

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
-import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  cache: new InMemoryCache(),
+	// uri: 'http://localhost:4000',
+	uri: "https://legao-graphql-cats.herokuapp.com/",
+	cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <GlobalStyles />
-    <Pages />
-  </ApolloProvider>,
-  document.getElementById('root')
+	<ApolloProvider client={client}>
+		<GlobalStyles />
+		<Pages />
+	</ApolloProvider>,
+	document.getElementById("root")
 );
