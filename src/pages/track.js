7645b72ddcc2fe985b699419353c2e0dde3f8c95
yikayiyo <837937787@qq.com>
2021-10-20 14:29:21 +0800

    update graphql schema fields

--- src/pages/track.js
+++ src/pages/track.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
-import { Layout, QueryResult } from '../components';
-import TrackDetail from '../components/track-detail';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import { Layout, QueryResult } from "../components";
+import TrackDetail from "../components/track-detail";
 
 /** GET_TRACK gql query to retrieve a specific track by its ID */
 export const GET_TRACK = gql`
-  query getTrack($trackId: ID!) {
-    track(id: $trackId) {
-      id
-      title
-      author {
-        id
-        name
-        photo
-      }
-      thumbnail
-      length
-      modulesCount
-      numberOfViews
-      modules {
-        id
-        title
-        length
-      }
-      description
-    }
-  }
+	query getTrack($trackId: ID!) {
+		track(id: $trackId) {
+			id
+			title
+			author {
+				id
+				name
+				photo
+			}
+			thumbnail
+			durationInSeconds
+			modulesCount
+			numberOfViews
+			modules {
+				id
+				title
+				durationInSeconds
+			}
+			description
+		}
+	}
 `;
 
 /**
@@ -33,17 +33,17 @@ export const GET_TRACK = gql`
  * and provides it to the TrackDetail component to display
  */
 const Track = ({ trackId }) => {
-  const { loading, error, data } = useQuery(GET_TRACK, {
-    variables: { trackId },
-  });
+	const { loading, error, data } = useQuery(GET_TRACK, {
+		variables: { trackId },
+	});
 
-  return (
-    <Layout>
-      <QueryResult error={error} loading={loading} data={data}>
-        <TrackDetail track={data?.track} />
-      </QueryResult>
-    </Layout>
-  );
+	return (
+		<Layout>
+			<QueryResult error={error} loading={loading} data={data}>
+				<TrackDetail track={data?.track} />
+			</QueryResult>
+		</Layout>
+	);
 };
 
 export default Track;
--- src/pages/tracks.js
+++ src/pages/tracks.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
-import TrackCard from '../containers/track-card';
-import { Layout, QueryResult } from '../components';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import TrackCard from "../containers/track-card";
+import { Layout, QueryResult } from "../components";
 
 /** TRACKS gql query to retreive all tracks */
 export const TRACKS = gql`
-  query getTracks {
-    tracksForHome {
-      id
-      title
-      thumbnail
-      length
-      modulesCount
-      author {
-        name
-        photo
-      }
-    }
-  }
+	query getTracks {
+		tracksForHome {
+			id
+			title
+			thumbnail
+			durationInSeconds
+			modulesCount
+			author {
+				name
+				photo
+			}
+		}
+	}
 `;
 
 /**
@@ -25,17 +25,17 @@ export const TRACKS = gql`
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  const { loading, error, data } = useQuery(TRACKS);
+	const { loading, error, data } = useQuery(TRACKS);
 
-  return (
-    <Layout grid>
-      <QueryResult error={error} loading={loading} data={data}>
-        {data?.tracksForHome?.map((track, index) => (
-          <TrackCard key={track.id} track={track} />
-        ))}
-      </QueryResult>
-    </Layout>
-  );
+	return (
+		<Layout grid>
+			<QueryResult error={error} loading={loading} data={data}>
+				{data?.tracksForHome?.map((track, _) => (
+					<TrackCard key={track.id} track={track} />
+				))}
+			</QueryResult>
+		</Layout>
+	);
 };
 
 export default Tracks;
