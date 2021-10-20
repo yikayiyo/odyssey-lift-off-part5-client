52673d66bace65e3b1ce28cb40b02bcb94bdb3a2
yikayiyo <837937787@qq.com>
2021-10-20 14:59:47 +0800

    use durationInSeconds on module detail page

--- src/pages/module.js
+++ src/pages/module.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
-import { Layout, ModuleDetail, QueryResult } from '../components';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
+import { Layout, ModuleDetail, QueryResult } from "../components";
 
 /**
  * GET_MODULE_AND_PARENT_TRACK gql query to retrieve a specific module and its parent track,
  * both needed for the ModuleDetail component
  */
 export const GET_MODULE_AND_PARENT_TRACK = gql`
-  query getModuleAndParentTrack($moduleId: ID!, $trackId: ID!) {
-    module(id: $moduleId) {
-      id
-      title
-      content
-      videoUrl
-    }
-    track(id: $trackId) {
-      id
-      title
-      modules {
-        id
-        title
-        length
-      }
-    }
-  }
+	query getModuleAndParentTrack($moduleId: ID!, $trackId: ID!) {
+		module(id: $moduleId) {
+			id
+			title
+			content
+			videoUrl
+		}
+		track(id: $trackId) {
+			id
+			title
+			modules {
+				id
+				title
+				durationInSeconds
+			}
+		}
+	}
 `;
 
 /**
@@ -31,17 +31,17 @@ export const GET_MODULE_AND_PARENT_TRACK = gql`
  * and feeds them to the ModuleDetail component
  */
 const Module = ({ moduleId, trackId }) => {
-  const { loading, error, data } = useQuery(GET_MODULE_AND_PARENT_TRACK, {
-    variables: { moduleId, trackId },
-  });
+	const { loading, error, data } = useQuery(GET_MODULE_AND_PARENT_TRACK, {
+		variables: { moduleId, trackId },
+	});
 
-  return (
-    <Layout fullWidth>
-      <QueryResult error={error} loading={loading} data={data}>
-        <ModuleDetail track={data?.track} module={data?.module} />
-      </QueryResult>
-    </Layout>
-  );
+	return (
+		<Layout fullWidth>
+			<QueryResult error={error} loading={loading} data={data}>
+				<ModuleDetail track={data?.track} module={data?.module} />
+			</QueryResult>
+		</Layout>
+	);
 };
 
 export default Module;
